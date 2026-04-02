04cd2618e781c174c517f1483fab30929ee42943
Ahmad Ibrahim <malikahmadibrahim332@gmail.com>
2026-04-02 18:09:22 +0500

    Added Card and Vmc

---
+++ src/components/inc/Vmc.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function VMC() {
+  return (
+    <section className="section bg-c-light border-top">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h3 className="main-heading">Vision, Mission and Values</h3>
+            <div className="underline mx-auto"></div>
+          </div>
+          <div className="col-md-4 text-container">
+            <h6>Our Vision</h6>
+            <p>
+              Lorem is simply dummy text of the printing and typestting
+              industry.
+            </p>
+          </div>
+          <div className="col-md-4 text-container">
+            <h6>Our Mission</h6>
+            <p>
+              Lorem is simply dummy text of the printing and typestting
+              industry.
+            </p>
+          </div>
+          <div className="col-md-4 text-container">
+            <h6>Our Values</h6>
+            <p>
+              Lorem is simply dummy text of the printing and typestting
+              industry.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default VMC;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { Button } from "bootstrap/dist/js/bootstrap.bundle.min";
 import Slider from "../inc/Slider";
 import { Link } from "react-router-dom";
+import VMC from "../inc/Vmc";
+import image from "../images/img3.jpg";
+
 function Home() {
   return (
     <div>
@@ -24,6 +27,32 @@ function Home() {
           </div>
         </div>
       </section>
+
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card shadow">
+                <img
+                  className="w-100 border-bottom"
+                  src={image}
+                  alt="Services"
+                />
+                <div className="card-body">
+                  <h6>Services 1</h6>
+                  <div className="underline"></div>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Autem recusandae consectetur repellat. Accusamus architecto
+                    quasi, consequuntur, distinctio quia recusandae a aspernatur
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <VMC />
     </div>
   );
 }
